2c797a98c1800a9425a73bae76c5f059f2f41823
sheergo <haddad.assaad71@gmail.com>
2024-09-15 15:30:52 +0300

    Update Main.vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,17 +1,16 @@
-
 <template>
 
   <input type="search" name="Search" id="" v-model="Search">
-   <p>Found {{ articleCount }} articles</p>
-  <div v-for="(article , index) in filteredArticles" :key="index" class="article">
+  <p>Found {{ articleCount }} articles</p>
+  <div v-for="(article, index) in filteredArticles" :key="index" class="article">
     <div class="title">
       <h2>
         {{ article.title }}
       </h2>
     </div>
-  {{ article.content }}
+    <span v-html="highlightText(article.content)"></span>
   </div>
- 
+
 
 </template>
 <script setup>
@@ -21,19 +20,29 @@ import { ref, computed } from 'vue'
 const Search = ref("")
 
 const filteredArticles = computed(() => {
-  return Articles.articles.filter(article => 
+  return Articles.articles.filter(article =>
     article.content.toLowerCase().includes(Search.value.toLowerCase())
   )
 })
 
 const articleCount = computed(() => filteredArticles.value.length)
+
+const highlightText = (text) => {
+  if (!Search.value) return text
+  const regex = new RegExp(`(${Search.value.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')})`, 'gi')
+  return text.replace(regex, '<span class="highlight">$1</span>')
+}
 </script>
 
 
 
-<style scoped>
+<style >
+.highlight {
+  background-color: yellow;
+  font-weight: bold;
+}
 
-.article{
+.article {
   width: 100%;
   height: fit-content;
   min-height: 50px;
@@ -43,7 +52,8 @@ const articleCount = computed(() => filteredArticles.value.length)
   margin-top: 2%;
 
 }
-.title{
+
+.title {
   font-weight: bold;
 }
 </style>
