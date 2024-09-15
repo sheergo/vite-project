18ad423138fb3e7a792bc665ce471026831754b6
sheergo <haddad.assaad71@gmail.com>
2024-09-15 15:18:48 +0300

    proj

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Main from './components/Main.vue'
+</script>
+
+<template>
+  <div class="main">
+    <Main/>
+  </div>
+  
+</template>
+
+<style scoped>
+.main{
+  width: 100vw;
+  padding: 0vw 1vw;
+  
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,49 @@
+
+<template>
+
+  <input type="search" name="Search" id="" v-model="Search">
+   <p>Found {{ articleCount }} articles</p>
+  <div v-for="(article , index) in filteredArticles" :key="index" class="article">
+    <div class="title">
+      <h2>
+        {{ article.title }}
+      </h2>
+    </div>
+  {{ article.content }}
+  </div>
+ 
+
+</template>
+<script setup>
+import Articles from '../assets/Articlejson.json'
+import { ref, computed } from 'vue'
+
+const Search = ref("")
+
+const filteredArticles = computed(() => {
+  return Articles.articles.filter(article => 
+    article.content.toLowerCase().includes(Search.value.toLowerCase())
+  )
+})
+
+const articleCount = computed(() => filteredArticles.value.length)
+</script>
+
+
+
+<style scoped>
+
+.article{
+  width: 100%;
+  height: fit-content;
+  min-height: 50px;
+  padding: 5px;
+  background-color: lightblue;
+  border: 1px solid black;
+  margin-top: 2%;
+
+}
+.title{
+  font-weight: bold;
+}
+</style>
